1af59ce757056ec9ddf124cc37ad164c7c9128b8
kaki-terra <quintaldoskitutes@gmail.com>
2025-10-31 13:39:48 -0300

    Fix: Remove arquivos do frontend duplicados no backend

--- BackEnd/components/SectionPlaceholder.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SectionPlaceholder: React.FC<{ className?: string }> = ({ className = 'min-h-[70vh]' }) => {
-  return (
-    <div className={`w-full ${className} bg-transparent`} aria-hidden="true" />
-  );
-};
-
-export default SectionPlaceholder;
